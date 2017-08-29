3df0eaef7a0fea2037aa5910c75bb4e0ad10ae9e
Bálint <s8.boxer@gmail.com>
2017-08-30 00:16:42 +0200

    Add files via upload

---
+++ WolfenMaze/main.js
@@ -0,0 +1,6 @@
+a = document.getElementById("canvas");
+b = document.body;
+c = a.getContext("2d");
+d = document;
+
+for(q='=1~=>Z.xY=(a,bXs(W)ZVXVWaYUk[++i]  )L+=),2,0Wu=v=3-3)%200,y=C(=0;|0]o(t(for("#Wx+1+1.map((q,.y(p,);,3,3Math.))	c.fill;if(Style=Rect(xx<u;x+yy<v;y+ &&g(nJ,d	)J=n";d.addEventListener("key"+u,(eVk[e.keyCode]=g=pZ(M[p||0)[pY|0;SXVM[a][aY]=b;sX=aV({x:a:b}CU+bY,a+boU/b,a/bh=i~V[W-0,-2W0,2)].filter(eZg(Ce	==it=pZWcoWpsin(p	;E(f=qZ{M=[...Array(33)]y,AVAxV!((x|(y	J=W1.5L=R~;p=W32;;;){S2A=h(pA[m=B=0]?j=CA[random()*A.length):B~B){=2)=2)g(	=~&&(I=h(D=2)[0])&&(pI,j=Dm=x=y=u)!m)break}S(o(Cj22p=j}})(k=[];u="down1u="up0setInterval((V{i=36;d=L30T=6e-2-TT;d=o(d,-1)JY+J>61)f(E+=5;333";u=4;0,0,u,u=3){r=J;a=L+(x-2)/u*R;lv=2e4;+)l+~e-2,rr,a240	,g(r)||([999",bbb",ddd"][(rY+r)%3,x,2-u/l/2,3,u/l)=v)}3;+)+)!M[y][x]?0":fff",x*3*3x=JY*3=J*3;0f0";Text("Score"+E,9,370x-1-193,93f";(wWxL0.4	)Y,w,2,2},16)';G=/[^!-T[-}]/.exec(q);)with(q.split(G))q=join(shift());eval(q)
